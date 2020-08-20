2fe342b9d8b7a1fab54e326df98672a0ea78bd4d
Vishal Bajoria <babynini@Vishals-MacBook-Pro.local>
2020-08-20 13:25:47 -0400

    make first change

--- src/App.js
+++ src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hare Krishna Hare Krishna Krishna Krishna Hare Hare. Hare Ram Hare Ram Ram Ram Hare Hare</h1>
     </div>
   );
 }
